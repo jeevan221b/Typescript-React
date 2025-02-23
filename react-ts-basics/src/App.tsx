c48dbec4fc50c2a3e3a19b4c4f55fb2331a596ca
Raj Diwakar <ard121001@gmail.com>
2025-02-23 21:58:10 +0530

    Added Header component and imported the index.css from course

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import CourseGoal from "./components/CourseGoal";
+import Header from "./components/Header";
+import HeaderAlien from "./assets/HeaderAlien.svg";
 
 function App() {
   return (
     <main>
+      <Header image={{ src: HeaderAlien, alt: "A List of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal title={"Learning Typescript and React"}>
         <p>Learning them from the ground up!!</p>
       </CourseGoal>
---
+++ react-ts-basics/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type HeaderProps = {
+  image: { src: string; alt: string };
+  children: ReactNode;
+};
+
+const Header = ({ image, children }: HeaderProps) => {
+  return (
+    <header>
+      <img src={image.src} alt={image.alt} />
+      {children}
+    </header>
+  );
+};
+
+export default Header;
